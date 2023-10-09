53bf2a3a7d29ab05066c48a41709867f9a834c5b
Yash Godara <yash17godara@gmail.com>
2023-10-09 17:24:54 +0530

    ui fix

--- app/ecom/zustand-ecommerce/page.js
+++ app/ecom/zustand-ecommerce/page.js
@@ -21,7 +21,7 @@ const LoginSection = () => {
     <>
       <div>
         <h2 className="text-xl font-bold underline px-4 py-2" >Login Section</h2>
-        <div className="flex flex-col md:flex-row gap-2">
+        <div className="flex flex-col md:flex-row gap-2 justify-center items-center text-center">
         <button className="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">Login</button>
         <button className="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">Logout</button>
         </div>
@@ -35,7 +35,7 @@ const CartSection = () => {
     <>
       <div>
         <h2 className="text-xl font-bold underline px-4 py-2" >Cart Section</h2>
-        <div className="flex flex-col md:flex-row gap-2">
+        <div className="flex flex-col md:flex-row gap-2 justify-center items-center text-center">
         <button className="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">Add Cart</button>
         <button className="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">Remove Cart</button>
         </div>
