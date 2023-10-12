c24bbd0a0dd00a2c2fce48dfa7a706d8e03a80cf
Yash Godara <yash17godara@gmail.com>
2023-10-12 12:36:20 +0530

    Update CakeContainer.jsx

--- part-1/src/components/CakeContainer.jsx
+++ part-1/src/components/CakeContainer.jsx
@@ -1,10 +1,30 @@
-function CakeContainer() {
+import { connect } from "react-redux";
+import { buyCake } from "../redux/cake/cakeAction";
+import PropTypes from 'prop-types';
+
+function CakeContainer(props) {
     return (
         <div>
-            <h2>Number of cakes</h2>
+            <h2>Number of cakes {props.numOfCakes}</h2>
             <button>Buy Cake</button>
         </div>
-    )
+    );
 }
 
-export default CakeContainer;
+CakeContainer.propTypes = {
+    numOfCakes: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
