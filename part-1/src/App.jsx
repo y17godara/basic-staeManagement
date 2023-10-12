4c336de21a717edd8253343ec369c17cb5d78c26
Yash Godara <yash17godara@gmail.com>
2023-10-12 11:46:07 +0530

    fix [.jsx]

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -1,12 +1,15 @@
-
+import { Provider } from "react-redux";
+import CakeContainer from "./components/CakeContainer";
+import store from "./redux/store";
 
 function App() {
-
   return (
     <>
-      Hello World
+      <Provider store={store}>
+        <CakeContainer />
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
